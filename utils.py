4e187e39ac6bdaab4f5889e246cc10a350b70707
KANABOON1 <1050970650@qq.com>
2026-07-22 23:21:47 +0800

    Reinitialize repo.

--- utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import torch
-def load_tokens(filename: str) -> torch.Tensor:
-
-    npt = np.load(filename)
-    npt = npt.astype(np.int32)
-    ptt = torch.tensor(npt, dtype=torch.int32)
-    return ptt
